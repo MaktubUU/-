524dcc003879f89d1ad36afe8ea7bf9ac7604e0a
天宇 蒋 <839097738@qq.com>
2023-08-15 14:10:24 +0800

    #

--- 6callonce.cpp
+++ 6callonce.cpp
@@ -2,11 +2,16 @@
 #include<mutex>
 #include<thread>
 using namespace std;
+static once_flag once;
+static Log *log = nullptr;
 //
 /*
 单例设计模式是一种常见的设计模式，用于确保某个类只能创建一个实例。
 由于单例实例是全局唯一的，因此在多线程环境中使用单例模式时，需要考虑线程安全的问题。
 下面是一个简单的单例模式的实现：
+
+call_once(once,func)可以保证func不管多少线程访问 只执行一次
+并且只能在线程里面使用、在main中使用会报错
 */
 class Log
 {
@@ -17,7 +22,7 @@ public:
 
     static Log& GetInstance()
     {
-        static Log log;//懒汉模式，
+        //static Log log;//懒汉模式，
 
         //static Log* log = nullptr;
         //if (!log)
@@ -25,34 +30,59 @@ public:
         //    log = new Log;
         //} 饿汉模式
 
-        return log;
+        if (!log) log = new Log;
+        call_once(once, init);
+        return *log;
+    }
+    static void init()
+    {
+        if (!log)
+        {
+            log = new Log;
+        }
     }
     void printlog(string msg)
     {
-        cout << msg << endl;
+        cout <<__TIME__<<" " << msg << endl;
     }
 };
 
 
-class Singleton {
-public:
-    static Singleton& getInstance() {
-        static Singleton instance;
-        return instance;
-    }
-    void setData(int data) {
-        m_data = data;
-    }
-    int getData() const {
-        return m_data;
-    }private:
-        Singleton() {}
-        Singleton(const Singleton&) = delete;
-        Singleton& operator=(const Singleton&) = delete;
-        int m_data = 0;
-};
-int main()
+//class Singleton {
+//public:
+//    static Singleton& getInstance() {
+//        static Singleton instance;
+//        return instance;
+//    }
+//    void setData(int data) {
+//        m_data = data;
+//    }
+//    int getData() const {
+//        return m_data;
+//    }private:
+//        Singleton() {}
+//        Singleton(const Singleton&) = delete;
+//        Singleton& operator=(const Singleton&) = delete;
+//        int m_data = 0;
+//};
+
+/*
+为什么使用单例模式 
+*/
+void print_error()
 {
+    Log::GetInstance().printlog("in thread");
+}
+int main06()
+{
+
+    Log::GetInstance().printlog("error");//单例模式
+
+    thread t1(print_error);
+    thread t2(print_error);
+    t1.join();
+    t2.join();
+    //同时调用的话，new log 会被执行两次
 
 
     return 0;
---
+++ 7生产者消费者.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<mutex>
+#include<thread>
+using namespace std;
+
+int main07()
+{
+
+
+	return 0;
+}
