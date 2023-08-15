dfe7bb55387885a1db573574ce6a257ad021c3bb
天宇 蒋 <839097738@qq.com>
2023-08-15 15:30:30 +0800

    #

--- 6callonce.cpp
+++ 6callonce.cpp
@@ -3,7 +3,7 @@
 #include<thread>
 using namespace std;
 static once_flag once;
-static Log *log = nullptr;
+//static Log * log = nullptr;
 //
 /*
 单例设计模式是一种常见的设计模式，用于确保某个类只能创建一个实例。
@@ -13,39 +13,39 @@ static Log *log = nullptr;
 call_once(once,func)可以保证func不管多少线程访问 只执行一次
 并且只能在线程里面使用、在main中使用会报错
 */
-class Log
-{
-public:
-    Log() {};
-    Log(const Log& log) = delete;
-    Log& operator=(const Log& log) = delete;
-
-    static Log& GetInstance()
-    {
-        //static Log log;//懒汉模式，
-
-        //static Log* log = nullptr;
-        //if (!log)
-        //{
-        //    log = new Log;
-        //} 饿汉模式
-
-        if (!log) log = new Log;
-        call_once(once, init);
-        return *log;
-    }
-    static void init()
-    {
-        if (!log)
-        {
-            log = new Log;
-        }
-    }
-    void printlog(string msg)
-    {
-        cout <<__TIME__<<" " << msg << endl;
-    }
-};
+//class Log
+//{
+//public:
+//    Log() {};
+//    Log(const Log& log) = delete;
+//    Log& operator=(const Log& log) = delete;
+//
+//    static Log& GetInstance()
+//    {
+//        static Log log;//懒汉模式，
+//
+//        //static Log* log = nullptr;
+//        //if (!log)
+//        //{
+//        //    log = new Log;
+//        //} 饿汉模式
+//
+//
+//        call_once(once, init);
+//        return (*log);
+//    }
+//    static void init()
+//    {
+//        //if (!log)
+//        {
+//            //log = new Log;
+//        }
+//    }
+//    void printlog(string msg)
+//    {
+//        cout <<__TIME__<<" " << msg << endl;
+//    }
+//};
 
 
 //class Singleton {
@@ -69,21 +69,21 @@ public:
 /*
 为什么使用单例模式 
 */
-void print_error()
-{
-    Log::GetInstance().printlog("in thread");
-}
-int main06()
-{
-
-    Log::GetInstance().printlog("error");//单例模式
-
-    thread t1(print_error);
-    thread t2(print_error);
-    t1.join();
-    t2.join();
-    //同时调用的话，new log 会被执行两次
-
-
-    return 0;
-}
+//void print_error()
+//{
+//    Log::GetInstance().printlog("in thread");
+//}
+//int main06()
+//{
+//
+//    Log::GetInstance().printlog("error");//单例模式
+//
+//    thread t1(print_error);
+//    thread t2(print_error);
+//    t1.join();
+//    t2.join();
+//    //同时调用的话，new log 会被执行两次
+//
+//
+//    return 0;
+//}
--- 7生产者消费者.cpp
+++ 7生产者消费者.cpp
@@ -1,11 +1,67 @@
 #include<iostream>
 #include<mutex>
 #include<thread>
+#include<condition_variable>
+#include<queue>
 using namespace std;
+/*
+condition_variable
 
-int main07()
+    创建一个 std::condition_variable 对象。
+    创建一个互斥锁 std::mutex 对象，用来保护共享资源的访问。
+    在需要等待条件变量的地方 使用 std::unique_lock<std::mutex> 对象锁定互斥锁
+
+    资源占用
+    并调用 std::condition_variable::wait()、
+    std::condition_variable::wait_for() 或 
+    std::condition_variable::wait_until() 函数等待条件变量。
+
+    资源解锁：
+    在其他线程中需要通知等待的线程时，
+    调用 std::condition_variable::notify_one() 或 
+    std::condition_variable::notify_all() 函数通知等待的线程。
+*/
+
+queue<int>q;
+condition_variable cv;
+mutex mtx;
+//要保证仓库在同一时间段只能进或出，给仓库加锁，
+
+void producer()
 {
+    //不断的生产
+    for (int i = 0; i < 100; i++)
+    {
+        unique_lock<mutex>lo(mtx);
+        q.push(i);
+        cv.notify_one();//创造完毕叫人来取
+        cout << "task:" << i << endl;
+        //lo.unlock();自动放锁
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
 
+}
+void consumer()
+{
+    //保持消费
+    while (true)
+    {
+        unique_lock<mutex>lo(mtx);
+        bool isempty = q.empty();
+        cv.wait(lo, []() {return !q.empty();});//队列空的时候等锁，
+        int value = q.front();
+        q.pop();
+        cout << "value:" << value << endl;
+    }
+
+}
+int main()
+{
+    /*cout << "dsad " << endl;*/
+    thread t1(producer);
+    thread t2(consumer);
+    t1.join();
+    t2.join();
 
 	return 0;
 }
